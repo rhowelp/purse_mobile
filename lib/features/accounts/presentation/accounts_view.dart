6b27e415a7cd31ed16fb5979ab2967769481acb0
rhowelp <rhowelp@gmail.com>
2025-02-14 10:01:18 +0800

    fix: update sizes of accounts

--- lib/features/accounts/presentation/accounts_view.dart
+++ lib/features/accounts/presentation/accounts_view.dart
@@ -39,18 +39,18 @@ class _AccountsViewState extends State<AccountsView> {
       icon: "assets/images/gcash.png",
       balance: 130000,
     ),
-    Account(
-      name: "GCash",
-      type: "Digital Cash",
-      icon: "assets/images/gcash.png",
-      balance: 130000,
-    ),
-    Account(
-      name: "GCash",
-      type: "Digital Cash",
-      icon: "assets/images/gcash.png",
-      balance: 130000,
-    ),
+    // Account(
+    //   name: "GCash",
+    //   type: "Digital Cash",
+    //   icon: "assets/images/gcash.png",
+    //   balance: 130000,
+    // ),
+    // Account(
+    //   name: "GCash",
+    //   type: "Digital Cash",
+    //   icon: "assets/images/gcash.png",
+    //   balance: 130000,
+    // ),
   ];
 
   @override
@@ -136,16 +136,28 @@ class _AccountsViewState extends State<AccountsView> {
               ),
             ),
           ),
-          const Spacer(),
-          _buildAccountList(),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: _buildAccountList(),
+            ),
+          ),
         ],
       ),
     );
   }
 
   Widget _buildAccountList() {
-    return Expanded(
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      ),
       child: Stack(
+        alignment: Alignment.bottomCenter,
         children: [
           // Positioned(
           //   top: 0,
@@ -153,16 +165,18 @@ class _AccountsViewState extends State<AccountsView> {
           //   right: 0,
           //   child: _buildAddAccountButton(),
           // ),
-          ListView.builder(
-            shrinkWrap: true,
-            padding: EdgeInsets.only(top: 40),
-            itemCount: accounts.length,
-            itemBuilder: (context, index) {
-              if (index == 0) {
-                return _buildAddAccountButton();
-              }
-              return _buildAccountItem(accounts[index], index);
-            },
+          ClipRRect(
+            child: ListView.builder(
+              shrinkWrap: true,
+              padding: EdgeInsets.only(top: 3),
+              itemCount: accounts.length,
+              itemBuilder: (context, index) {
+                if (index == 0) {
+                  return _buildAddAccountButton();
+                }
+                return _buildAccountItem(accounts[index], index);
+              },
+            ),
           ),
         ],
       ),
@@ -171,7 +185,7 @@ class _AccountsViewState extends State<AccountsView> {
 
   Widget _buildAddAccountButton() {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 20),
       decoration: const BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.only(
@@ -211,7 +225,7 @@ class _AccountsViewState extends State<AccountsView> {
     return Transform.translate(
       offset: Offset(0, -index * 12),
       child: Container(
-        padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 15),
         decoration: const BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.only(
@@ -222,7 +236,7 @@ class _AccountsViewState extends State<AccountsView> {
             BoxShadow(
               color: Colors.black12,
               blurRadius: 5,
-              spreadRadius: 2,
+              spreadRadius: 0,
               offset: Offset(0.0, -2),
             ),
           ],
@@ -248,217 +262,3 @@ class _AccountsViewState extends State<AccountsView> {
     );
   }
 }
-
-// class AccountScreen extends StatefulWidget {
-//   const AccountScreen({super.key});
-
-//   @override
-//   State<AccountScreen> createState() => _AccountScreenState();
-// }
-
-// class _AccountScreenState extends State<AccountScreen> {
-//   final List<Account> accounts = [
-//     Account(
-//         name: "BDO Unibank",
-//         type: "Savings",
-//         balance: "₱16,000",
-//         logo: "assets/images/bdo.png"),
-//     Account(
-//         name: "BPI",
-//         type: "Credit card",
-//         balance: "₱130,000",
-//         logo: "assets/images/bpi.png"),
-//     Account(
-//         name: "GCash",
-//         type: "Digital Cash",
-//         balance: "₱130,000",
-//         logo: "assets/images/gcash.png"),
-//   ];
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.grey[200],
-//       body: Column(
-//         children: [
-//           const SizedBox(height: 50),
-//           BalanceCard(),
-//           const SizedBox(height: 20),
-//           Expanded(
-//             child: Stack(
-//               children: [
-//                 Positioned(
-//                   top: 0,
-//                   left: 20,
-//                   right: 20,
-//                   child: AddAccountButton(),
-//                 ),
-//                 ListView.builder(
-//                   padding: EdgeInsets.only(top: 40),
-//                   itemCount: accounts.length,
-//                   itemBuilder: (context, index) {
-//                     return AccountCard(account: accounts[index], index: index);
-//                   },
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// class Account {
-//   final String name;
-//   final String type;
-//   final String balance;
-//   final String logo;
-
-//   Account(
-//       {required this.name,
-//       required this.type,
-//       required this.balance,
-//       required this.logo});
-// }
-
-// class BalanceCard extends StatelessWidget {
-//   const BalanceCard({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(horizontal: 20),
-//       child: Container(
-//         padding: EdgeInsets.all(20),
-//         decoration: BoxDecoration(
-//           gradient: LinearGradient(
-//             colors: [Colors.blue.shade900, Colors.green.shade700],
-//           ),
-//           borderRadius: BorderRadius.circular(15),
-//         ),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             Text(
-//               "Total Balance",
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontSize: 14,
-//               ),
-//             ),
-//             SizedBox(height: 10),
-//             Text(
-//               "₱616,000.00",
-//               style: TextStyle(
-//                 color: Colors.white,
-//                 fontSize: 28,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class AddAccountButton extends StatelessWidget {
-//   const AddAccountButton({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-//       decoration: BoxDecoration(
-//         color: Colors.white,
-//         borderRadius: BorderRadius.circular(15),
-//         boxShadow: [
-//           BoxShadow(
-//             color: Colors.black26,
-//             blurRadius: 5,
-//             offset: Offset(0, 3),
-//           )
-//         ],
-//       ),
-//       child: Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//         children: [
-//           Row(
-//             children: [
-//               Icon(Icons.add_circle, color: Colors.green),
-//               SizedBox(width: 10),
-//               Text(
-//                 "Add Account",
-//                 style: TextStyle(
-//                   color: Colors.green,
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//               ),
-//             ],
-//           ),
-//           Icon(
-//             Icons.visibility_off,
-//             color: Colors.grey,
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-// class AccountCard extends StatelessWidget {
-//   final Account account;
-//   final int index;
-
-//   const AccountCard({super.key, required this.account, required this.index});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Transform.translate(
-//       offset: Offset(0, -index * 25),
-//       child: Padding(
-//         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-//         child: Container(
-//           padding: EdgeInsets.all(15),
-//           decoration: BoxDecoration(
-//             color: Colors.white,
-//             borderRadius: BorderRadius.circular(25),
-//             boxShadow: [
-//               BoxShadow(
-//                 color: Colors.black26,
-//                 blurRadius: 5,
-//                 offset: Offset(0, 3),
-//               )
-//             ],
-//           ),
-//           child: Row(
-//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//             children: [
-//               Row(
-//                 children: [
-//                   CircleAvatar(
-//                     backgroundImage: AssetImage(account.logo),
-//                     radius: 20,
-//                   ),
-//                   SizedBox(width: 10),
-//                   Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: [
-//                       Text(account.name,
-//                           style: TextStyle(
-//                               fontSize: 16, fontWeight: FontWeight.bold)),
-//                       Text(account.type, style: TextStyle(color: Colors.grey)),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//               Text(account.balance,
-//                   style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
